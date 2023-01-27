e2ada637f0f387310398da56e45ce839465e9abc
chuahce <choongeng.chuah@gmail.com>
2023-01-27 16:36:15 +0800

    Final Commit for Submission

---
+++ task01/src/sdf/Main.java
@@ -0,0 +1,57 @@
+package sdf;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Set;
+import java.util.AbstractMap;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    String fileName = args[0];
+
+    System.out.printf("Processing %s\n", fileName);
+
+    FileReader fr = new FileReader(fileName);
+    BufferedReader br = new BufferedReader(fr);
+
+    Integer totalWords = 0;
+    Map<String, Integer> wordMap = new HashMap<>();
+
+    for (Integer i = 1; i <= 100; i++) {
+      String line = br.readLine();
+      if (null == line)
+        break;
+      String[] words = line.trim().split(" ");
+      totalWords += words.length;
+      for (String w : words) {
+        String clearWord = w.replaceAll(",", "");
+        Integer v = wordMap.getOrDefault(clearWord, 0);
+        v++;
+        wordMap.put(clearWord, v);
+      }
+    }
+
+    br.close();
+    fr.close();
+
+    Set<String> uniqueWords = wordMap.keySet();
+
+    PriorityQueue<Map.Entry<String, Double>> pq = new PriorityQueue<>((a, b) -> (int) (b.getValue() - a.getValue()));
+
+    for (String w : uniqueWords) {
+      double termFrequency = (double) wordMap.get(w) / totalWords;
+      pq.add(new AbstractMap.SimpleEntry<>(w, termFrequency));
+    }
+
+    System.out.println("Top 10 words based on term frequency:");
+    for (int i = 1; i <= 10; i++) {
+      Map.Entry<String, Double> entry = pq.poll();
+      System.out.println(i + ". " + entry.getKey() + " - " + entry.getValue());
+    }
+  }
+}
+
---
+++ task02/src/sdf/Main.java
@@ -0,0 +1,55 @@
+package sdf;
+
+import java.io.Console;
+
+public class Main {
+  public static void main(String[] args) {
+    Console cons = System.console();
+    System.out.println("Welcome.");
+    String equation = "";
+    float result = 0.0f;
+
+    while (!equation.equals("exit")) {
+      equation = cons.readLine("> ");
+      if (equation.equals("exit")) {
+        System.out.println("Bye bye");
+        break;
+      }
+
+      if (equation.contains("$last")) {
+        equation = equation.replace("$last", String.format("%f", result));
+      }
+
+      String[] parts = equation.split(" ");
+      if (parts.length != 3) {
+        System.out.println("Invalid equation format. Please enter in the form of 'number operator number'");
+        continue;
+      }
+      try {
+        float num1 = Float.parseFloat(parts[0]);
+        float num2 = Float.parseFloat(parts[2]);
+        char operator = parts[1].charAt(0);
+        switch (operator) {
+          case '+':
+            result = num1 + num2;
+            break;
+          case '-':
+            result = num1 - num2;
+            break;
+          case '*':
+            result = num1 * num2;
+            break;
+          case '/':
+            result = num1 / num2;
+            break;
+          default:
+            System.out.println("Invalid operator");
+            continue;
+        }
+        System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
+      } catch (NumberFormatException e) {
+        System.out.println("Enter in the following format: number space operator space number");
+      }
+    }
+  }
+}
